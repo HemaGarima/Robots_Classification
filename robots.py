ed174acb8a9d6bd52861cb306303dae0f9726c1e
Hema <hemlatasharma23052003@gmail.com>
2024-09-15 22:55:03 +0530

    Creating streamlit app for robots classification

---
+++ robots.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from joblib import load
+
+st.title("Classification of Robots from their conversation")
+
+model = load('robots.joblib')
+
+numbers = []
+
+for i in range(1, 11):
+    num = st.number_input(f"Enter number {i} (between 0 and 10103494901760)", min_value=0, max_value=10103494901760, key=f'num_{i}')
+    numbers.append(num)
+
+clicked = st.button("Find Robot")
+
+if(clicked == True):
+    st.write("You have entered the following numbers:")
+    st.write(numbers)
+    pred = model.predict([numbers])
+    print(pred[0])
+
+    st.write(f"Robot {pred[0]} have said these numbers")
